17428b9f3d7c2efc54caf0c3cefaf4152d7172a6
lucien <44745575+lucien2018@users.noreply.github.com>
2021-01-28 20:57:16 +0000

    des reglage supplementaire

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ClientApp',
+    title: 'e-bestteam',
     htmlAttrs: {
       lang: 'en'
     },
@@ -111,5 +111,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+   
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,355 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ClientApp
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+
+<main id="main">
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>A propos</h2>
+        <h3>Appenez plus <span>sur nous</span></h3>
+        <p>E-bestTeam est une initiative de jeunes etudiants talentueux qui desirent apporter leurs pierre a l'edifice de ce monde qui est en plein construction </p>
       </div>
+
+      <div class="row content">
+        <div class="col-lg-6">
+          <p style="text-align:justify;">
+            Face aux realités d'un monde permmanenment en mutation, nous n'aimerions pas que l'afrique, plus précisement la cote d'ivoire, soit en marge de ces grands tournents de l'histoire de l'humanité. C'est la raison pour laquelle nous proposons de repondre le mieux possible à vos besoins au travers des moyens revolutionnaires, multiples et divers qu'offre les sciences de l'informatiques. Ce sont entre autres :
+          </p>
+          <ul>
+            <li><i class="ri-check-double-line"></i> la creation d'application web ( E-commerce, E-learning, ...)</li>
+            <li><i class="ri-check-double-line"></i> la creation d'application desktop et mobiles</li>
+            <li><i class="ri-check-double-line"></i> la maintenance des systeme informatises </li>
+            <li><i class="ri-check-double-line"></i> etc ... </li>
+          </ul>
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0">
+          <p style="text-align:justify;">
+            Aussi, parce que nous croyons dans des perspectives d'une grande guerre de données informatiques qui est à venir, nous désirons transmettre nos connaissances moyennant des formations afin d'élever une élite, armée et prête pour le combat  
+          </p>
+          <div class="text-center"><a href="#" class="btn-learn-more">Nos formations</a></div>
+        </div>
+      </div>
+
     </div>
-  </div>
+  </section><!-- End About Section -->
+
+  <!-- ======= Services Section ======= -->
+  <section id="services" class="services contact">
+    <div class="container-fluid">
+
+      <div class="section-title">
+        <h2>Services</h2>
+        <h3>Nous proposons plusieurs <span>Services</span></h3>
+        <p>Nos services sont de qualité et nous esperons que vous en tirerez satisfaction</p>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+          <div class="icon-box content">
+            <div class="icon"></div>
+            <h4 class="title"><a href="">Developpement d'Application</a></h4>
+            <div style="text-align:justify;">
+              <p class="description">Nous mettons a votre disposition une equipe dynamique de developpeurs pour vos besoins :</p>
+              <ul class="description">
+                <li>De création d'applications web ( E-commerce, E-learning, ...)</li>
+                <li>De création d'applications desktop et mobiles</li>
+                <li>etc ... </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+          <div class="icon-box content">
+            <div class="icon"></div>
+            <h4 class="title"><a href="">Securite Informatique</a></h4>
+            <div style="text-align:justify;">
+              <p class="description">Nous avons a notre disposition des pentesters pour :</p>
+              <ul class="description">
+                <li> la verification de la solidité de votre systeme informatique</li>
+                <li>la securisation de vos données contre des intrusions</li>
+                <li> etc ... </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+          <div class="icon-box" >
+            <div class="icon"></div>
+            <h4 class="title"><a href="">Reseau informatique</a></h4>
+            <div style="text-align:justify;">
+              <p class="description">Nos experts reseau oeuvront pour repondre  efficecement et dans les plus bref delai a vous soucis :</p>
+              <ul class="description">
+                <li> De creation ou de deploiement reseaux </li>
+                <li> De maintenance reseau</li>
+                <li> etc ... </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+          <div class="icon-box">
+            <div class="icon"></div>
+            <h4 class="title"><a href="">Maintenance Informatique</a></h4>
+            <div style="text-align:justify;">
+              <p class="description">Un outil défaillant dans votre systeme informatique peut engendrer de grave consequence, nos experts maintenanciers sont a votre disposition pour :</p>
+              <ul class="description">
+                <li>La maintenance de vos ordinateurs et de tout autres appareils intervenant dans votre systeme informatique</li>
+                <li>etc ... </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</br>
+    <p class="text-center">Vous désirez un service, surtout n'hesitez pas à remplir ce formulaire de prise de rendez-vous et nous en discuterons tranquillement autour d'un café. </p>
+</br>
+
+    <div class="text-center"><a class="prd" href="#" data-bs-toggle="modal" data-bs-target="#example1Modal">Prendre un Rendez-vous</a></div>
+
+              <!-- Modal -->
+        <div class="modal fade text-center" id="example1Modal" tabindex="-1" aria-labelledby="example1ModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQIF-ZPzqUEi6Q1HZ4NLFAdNjEH28o6qFYwD536mpu50tfbA/viewform?embedded=true" width="640" height="1008" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
+                </br>
+                <p style="color:#000000;"> <span style="color:red;">NB:</span> Remplissez convenablement ce formulaire et nous vous contacterons pour coordonner la rencontre</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+ 
+  </section><!-- End Services Section -->
+
+ 
+
+  <!-- ======= Cta Section ======= -->
+  <section id="cta" class="cta contact">
+    <div class="container ">
+    
+      <div class="text-center">
+        <h3>Nos formations</h3>
+        <p>Nos formations couvrent la totalité des connaissances réquises pour etre opérationnel en entreprise.</p>
+        <p>Si vous vous interessez a nos formations</p>
+
+        <a class="cta-btn" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">remplissez ce formalaire s'il vous plait</a>
+
+          <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfNZQPPw6eEg46Qzle_fZhEfdvzMua9DDQ0bEObzZjvrtB3IQ/viewform?embedded=true" width="640" height="868" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </br>
+    </br>
+        <marquee><p style="color:white;"><span style="color:red;">NB:</span> Nous ne delivrons pour l'instant aucun diplôme mais nous vous guarantissons l'obtient de competences practiques.</p></marquee>
+    </div>
+  </section><!-- End Cta Section -->
+
+  
+
+  <!-- ======= Team Section ======= -->
+  <section id="team" class="team">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Notre Equipe</h2>
+        <h3>Dynamique et travailleuse est <span>notre equipe</span></h3>
+        <p>Comme votre satisfaction passe par le travail bien-fait, nous faisons  a E-BestTeam, la visée de l'excellence notre prioritée  </p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-lg-3 col-md-6 col-sm-12 d-flex align-items-stretch">
+          <div class="member">
+            <div class="member-img">
+              <img src="~/assets/img/team/profils.png" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="icofont-telegram"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-whatsapp"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>KOBENA Kouame Lucien</h4>
+              <span>Lead des prestations des services de creation d'application</span>
+            </br>
+              <a href="/kobena" target="_blank" class="prd">Voir CV </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12 d-flex align-items-stretch">
+          <div class="member">
+            <div class="member-img">
+              <img src="~/assets/img/team/profils.png" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="icofont-telegram"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-whatsapp"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>Sarah Jhonson</h4>
+              <span>Lead des prestations des services reseau</span>
+            </br>
+              <a href="CV/index1.html"  target="_blank" class="prd">Voir CV </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12 d-flex align-items-stretch">
+          <div class="member">
+            <div class="member-img">
+              <img src="~/assets/img/team/profils.png" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="icofont-telegram"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-whatsapp"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>William Anderson</h4>
+              <span>lead des prestations des sevices de Securité informatique </span>
+            </br>
+            <a href="CV/index2.html" class="prd">Voir CV </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12 d-flex align-items-stretch">
+          <div class="member">
+            <div class="member-img">
+              <img src="~/assets/img/team/profils.png" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="icofont-telegram"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-whatsapp"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>Amanda Jepson</h4>
+              <span>lead des prestations de service de Maintenance Informatique </span>
+            </br>
+              <a href="CV/index3.html" class="prd">Voir CV</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Team Section -->
+
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <h3>Contactez <span>Nous</span></h3>
+        <p>Nous sommes a votre Dispositions, vour pouvez nous laisser un mail</p>
+      </div>
+
+      <div>
+        <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div class="row mt-5">
+
+        <div class="col-lg-4">
+          <div class="info">
+            <div class="address">
+              <i class="icofont-google-map"></i>
+              <h4>Localisation: </h4>
+              <p>ABIDJAN Yopougon</p>
+              <p>Niangon à droite</p>
+            </div>
+
+            <div class="email">
+              <i class="icofont-envelope"></i>
+              <h4>Email:</h4>
+              <p>ebestteam@gmail.com</p>
+            </div>
+
+            <div class="phone">
+              <i class="icofont-phone"></i>
+              <h4>telephone :</h4>
+              <p>+225 48 60 87 03</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col-lg-8 mt-5 mt-lg-0">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeZas7lZPq6DFUvWdzfDJhtwYf9Ib5Z2dUTmjcpoxujNOSBLQ/viewform?embedded=true" width="640" height="920" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
+
+      <!--<form action="" method="post" role="form" class="php-email-form">
+            <div class="row">
+              <div class="col-md-6 form-group">
+                <input type="text" name="nom" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validate"></div>
+              </div>
+              <div class="col-md-6 form-group mt-3 mt-md-0">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                <div class="validate"></div>
+              </div>
+            </div>
+            <div class="form-group mt-3">
+              <input type="text" class="form-control" name="sujets" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+              <div class="validate"></div>
+            </div>
+            <div class="form-group mt-3">
+              <textarea class="form-control" name="messages" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+              <div class="validate"></div>
+            </div>
+            <div class="mb-3">
+              <div class="loading">Loading</div>
+              <div class="error-message"></div>
+              <div class="sent-message">Your message has been sent. Thank you!</div>
+            </div>
+            <div class="text-center"><button type="submit">Envoyez Message</button></div>
+          </form>-->
+
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Contact Section -->
+
+</main><!-- End #main -->
 </template>
 
 <script>
-export default {}
+export default {
+  layout: 'bestteam'
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
